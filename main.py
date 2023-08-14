edddef067bd9ba690949882af9b22d315b96f053
koni4045 <115186907+koni4045@users.noreply.github.com>
2023-08-14 02:33:13 -0400

    v2
    version2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 import cv2
 import insightface
+from PIL import Image
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from insightface.app import FaceAnalysis
 
@@ -18,8 +19,15 @@ def uploaded_file(filename):
     return send_from_directory('uploads', filename)
 
 
-@app.route('/imageDisplay', methods=['GET', 'POST'])
-def display_image():
+@app.route('/faces/<filename>')
+def uploaded_faces(filename):
+    return send_from_directory('faces', filename)
+
+
+@app.route('/face_detect_and_select', methods=['GET', 'POST'])
+def display_faces():
+    clear_folder('faces')
+    clear_folder('uploads')
     if 'source' not in request.files:
         return "No image part in the form"
 
@@ -27,25 +35,43 @@ def display_image():
     if source.filename == '':
         return "No selected image"
 
-    # You can process the image here or save it to a desired location
-    # For example, to save the image to the 'uploads' folder:
     source.save('uploads/' + source.filename)
 
+    faces = detect_faces('uploads/' + source.filename)
+    filenames = []
+    for i, face in enumerate(faces):
+        cropped_image = (Image.open('uploads/' + source.filename)).crop(face['bbox'])
+        filename = 'faces/face' + str(i + 1) + '.jpg'
+        filenames.append('face' + str(i + 1) + '.jpg')
+        cropped_image.save(filename)
+    print(filenames)
+    # result_image = swap_faces(detect_faces('uploads/' + target.filename), source_face, 'uploads/' + target.filename)
+    # cv2.imwrite('uploads/result.jpg', result_image)
+    return render_template('face_detect_and_select.html', filenames=filenames)
+
+
+@app.route('/target_face_upload_and_swap', methods=['GET', 'POST'])
+def target_face_upload_and_swap():
+    face_number = -1
+    if request.method == 'POST':
+        selected_value = request.form.get('selected_file')  # Get the selected dropdown value
+        face_number = int(selected_value[-1]) - 1
+        print(face_number)
+        # Use the selected_value as needed
     if 'target' not in request.files:
         return "No image part in the form"
-
+    print(os.listdir('uploads')[0])
+#source = Image.open('uploads/' + os.listdir('uploads')[0])
+    source_face = detect_faces('uploads/' + os.listdir('uploads')[0])[face_number]
     target = request.files['target']
     if target.filename == '':
         return "No selected image"
 
-    # You can process the image here or save it to a desired location
-    # For example, to save the image to the 'uploads' folder:
     target.save('uploads/' + target.filename)
-    faces = detect_faces('uploads/' + source.filename)
-    source_face = faces[0]
+
     result_image = swap_faces(detect_faces('uploads/' + target.filename), source_face, 'uploads/' + target.filename)
     cv2.imwrite('uploads/result.jpg', result_image)
-    return render_template('image_display.html', source_path=source.filename, target_path=target.filename,
+    return render_template('result_image.html', target_path=target.filename,
                            image_filename='result.jpg')
 
 
@@ -67,6 +93,21 @@ def swap_faces(faces, source_face, image_path):
     return res
 
 
+def clear_folder(folder_path):
+    try:
+        # List all files in the folder
+        file_list = os.listdir(folder_path)
+
+        # Loop through the files and delete each one
+        for file_name in file_list:
+            file_path = os.path.join(folder_path, file_name)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+
+        return True  # Successfully cleared all images
+    except Exception as e:
+        return False  # An error occurred
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
